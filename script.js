11b4a070e2d0bed8759bce2627475a83325392e4
Marco-Morales007 <brosly3@live.com.mx>
2023-08-26 14:14:21 -0600

    Practica 3 (No completa)

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 
-var texto = document.getElementById('text1');
+var usuario = document.getElementById('usuario');
+var password = document.getElementById('password');
 
-function mostrarAlerta(){
-    alert(texto.value);
+var p = document.createElement("p");
+p.setAttribute("id","p1");
+
+function login(){
+    document.getElementById('p1').value = "";
+    var pTexto = document.createTextNode("El usuario " + usuario.value + " inicio sesion con exito...");
+    p.appendChild(pTexto);
+    document.body.appendChild(p);
+    limpiarCampos();
 }
 
-function limpiarTexto(){
-    texto.value = "";
+
+function signup(){
+    document.getElementById('p1').value = "";
+    var pTexto = document.createTextNode("El usuario " + usuario.value + " se registro con exito...");
+    p.appendChild(pTexto);
+    document.body.appendChild(p);
+    limpiarCampos();
+}
+
+function limpiarCampos(){
+    usuario.value = "";
+    password.value = "";
+       
 }
