184f12d483f180bb34e0aaeb4b805a66b8082b46
Marco-Morales007 <brosly3@live.com.mx>
2023-09-02 13:03:58 -0600

    Practica 4

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
 
-var usuario = document.getElementById('usuario');
-var password = document.getElementById('password');
+var tipoGasto = document.getElementById('tipoGasto');
+var monto = document.getElementById('monto');
+var gastos = [];
 
-var p = document.createElement("p");
-p.setAttribute("id","p1");
-
-function login(){
-    document.getElementById('p1').value = "";
-    var pTexto = document.createTextNode("El usuario " + usuario.value + " inicio sesion con exito...");
-    p.appendChild(pTexto);
-    document.body.appendChild(p);
-    limpiarCampos();
-}
-
-
-function signup(){
-    document.getElementById('p1').value = "";
-    var pTexto = document.createTextNode("El usuario " + usuario.value + " se registro con exito...");
-    p.appendChild(pTexto);
-    document.body.appendChild(p);
-    limpiarCampos();
+function agregarGasto(){
+    gastos.push(tipoGasto.value + ":" + "$" + monto.ariaValueText);
 }
 
 function limpiarCampos(){
-    usuario.value = "";
-    password.value = "";
-       
+    tipoGasto.value = "";
+    monto.value = "";
+    console.log(gastos);    
 }
